046cefda7126652541bd2bd76089894178f6cdfb
lbyjwwyqt147 <lbyjwwyqt147@163.com>
2019-03-26 23:13:24 +0800

    dict

--- src/main/java/pers/liujunyi/cloud/core/configuration/FilterConfiguration.java
+++ src/main/java/pers/liujunyi/cloud/core/configuration/FilterConfiguration.java
@@ -1,6 +1,5 @@
 package pers.liujunyi.cloud.core.configuration;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -16,21 +15,13 @@ import pers.liujunyi.cloud.common.encrypt.filter.SignAuthFilter;
 @Order(20)
 public class FilterConfiguration {
 
-    /**  AES 密匙 */
-    @Value("${spring.encrypt.secretKey}")
-    private String secretKey;
-
-    /**  签名过期分钟数  */
-    @Value("${spring.encrypt.signExpireTime}")
-    private Integer signExpireMinute;
-
     /**
      *  签名认证过滤器
      * @return
      */
     @Bean
     public SignAuthFilter signAuthFilter() {
-        return new SignAuthFilter(this.secretKey, this.signExpireMinute);
+        return new SignAuthFilter();
     }
 
     /**
@@ -40,8 +31,17 @@ public class FilterConfiguration {
     @Bean
     public FilterRegistrationBean signAuthFilterRegistration(
             SignAuthFilter signAuthFilter) {
-        FilterRegistrationBean bean = new FilterRegistrationBean(signAuthFilter);
-        bean.setOrder(1);
-        return bean;
+        FilterRegistrationBean registrationBean = new FilterRegistrationBean(signAuthFilter);
+        registrationBean.addUrlPatterns("/*");
+        //过滤应用程序中所有资源,当前应用程序根下的所有文件包括多级子目录下的所有文件，注意这里*前有“/”
+        //registration.addUrlPatterns("/*");
+        //过滤指定的类型文件资源, 当前应用程序根目录下的所有html文件，注意：*.html前没有“/”,否则错误
+       // registration.addUrlPatterns(".html");
+        //过滤指定的目录下的所有文件,当前应用程序根目录下的folder_name子目录（可以是多级子目录）下所有文件
+       // registration.addUrlPatterns("/folder_name/*");
+        //过滤指定文件
+       // registration.addUrlPatterns("/index.html");
+        registrationBean.setOrder(5);
+        return registrationBean;
     }
 }
