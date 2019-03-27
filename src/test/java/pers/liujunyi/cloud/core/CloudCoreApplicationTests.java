0efdb8d039a68bc824d80585840de87afe175de2
lbyjwwyqt147 <lbyjwwyqt147@163.com>
2019-03-27 21:31:49 +0800

    dict

---
+++ src/test/java/pers/liujunyi/cloud/core/CloudCoreApplicationTests.java
@@ -0,0 +1,17 @@
+package pers.liujunyi.cloud.core;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CloudCoreApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
+
